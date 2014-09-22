c489f66c0f8b398edaf2228f9bad3d7dfd8ee1a9
Chaoran Yang <chaorany@me.com>
2014-09-22 15:25:13 -0500

    Add macro GLOBALS_ALIGNED_RANGE.

--- globals.h
+++ globals.h
@@ -7,5 +7,6 @@ extern char __data_start, _end;
 
 #define GLOBALS_ALIGNED_ADDR (PAGE_ALIGN_DOWN(&__data_start))
 #define GLOBALS_ALIGNED_SIZE (PAGE_ALIGN_UP  (&_end) - GLOBALS_ALIGNED_ADDR)
+#define GLOBALS_ALIGNED_RANGE GLOBALS_ALIGNED_ADDR, GLOBALS_ALIGNED_SIZE
 
 #endif /* end of include guard: GLOBALS_H */
