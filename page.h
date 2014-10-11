8c2382d076c3078bdf6b45af2523092dc2a513dd
Chaoran Yang <chaorany@me.com>
2014-10-11 01:27:04 -0500

    Add file that is accidentally removed.

---
+++ page.h
@@ -0,0 +1,25 @@
+#ifndef PAGE_H
+#define PAGE_H
+
+#include <stddef.h>
+
+#define PAGE_SIZE      4096ULL
+#define PAGE_SIZE_MASK (PAGE_SIZE - 1)
+
+#define PAGE_ALIGN_DOWN(x) (\
+    (void *) ((size_t) (x) & ~PAGE_SIZE_MASK) \
+)
+
+#define PAGE_ALIGN_UP(x) (\
+    (void *) ((size_t) ((x) + PAGE_SIZE_MASK) & ~PAGE_SIZE_MASK) \
+)
+
+#define PAGE_ALIGNED(x) (\
+    0 == ((size_t) (x) & PAGE_SIZE_MASK) \
+)
+
+#define PAGE_DIVISIBLE(x) (\
+    0 == ((x) % PAGE_SIZE) \
+)
+
+#endif /* end of include guard: PAGE_H */
