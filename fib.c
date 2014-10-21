f0560b12858847636a3383d62cb438e1c6e1a3b3
Chaoran Yang <chaorany@me.com>
2014-10-21 14:31:55 -0500

    For test purpose.

--- fib.c
+++ fib.c
@@ -5,12 +5,15 @@ unsigned long fib(unsigned long n)
   if (n < 2) return n;
 
   unsigned long m;
-  FIBRIL_FORK(m = fib(n - 1), (int, n));
+  FIBRIL_FORK(m, fib(n - 1), (int, n));
 
   return m + fib(n - 2);
 }
 
 int main(int argc, const char *argv[])
 {
-  return fib(42);
+  fibril_init(4);
+  fibril_exit();
+  return 0;
+  /*return fib(42);*/
 }
