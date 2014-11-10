477205a21c0f53686d5fb941f8a857404d936fdc
Chaoran Yang <chaorany@me.com>
2014-11-10 14:23:40 -0600

    Better safe.h tools.

--- safe.h
+++ safe.h
@@ -9,19 +9,25 @@
 #define SAFE_TOSTRING(x) SAFE_STRINGIFY(x)
 #define SAFE_AT __FILE__ ":" SAFE_TOSTRING(__LINE__) ": "
 
-#define SAFE_CALL(err, call) do { \
-  int T = ((intptr_t) err == (intptr_t) (call)); \
-  if (T) { \
+#define SAFE_ASSERT(cond) do { \
+  if (!(cond)) { \
     DEBUG_DUMP(0, "error: " SAFE_AT "%m"); \
-    DEBUG_BREAK(T); \
+    DEBUG_BREAK(!(cond)); \
   } \
-} while(0)
+} while (0)
 
 #else
-#define SAFE_CALL(err, call) do { call; } while (0)
+#define SAFE_ASSERT(...)
 #endif
 
-#define SAFE_NNCALL(call) SAFE_CALL(-1, call);
-#define SAFE_NZCALL(call) SAFE_CALL(0, call);
+#define SAFE_NNCALL(call) do { \
+  intptr_t ret = (intptr_t) (call); \
+  SAFE_ASSERT(ret >= 0); \
+} while (0)
+
+#define SAFE_NZCALL(call) do { \
+  intptr_t ret = (intptr_t) (call); \
+  SAFE_ASSERT(ret != 0); \
+} while (0)
 
 #endif /* end of include guard: SAFE_H */
