903248d9f61643feb4439105fc5ceb6e30bbd1eb
Chaoran Yang <chaorany@me.com>
2014-11-25 16:53:35 -0600

    Clean slate.

--- config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define PAGE_SIZE 4096ULL
-#define MAX_PROCS 256
-#define FILENAME_LIMIT 256
-#define N_SHMAP_ENTRIES 4096
-
-#endif /* end of include guard: CONFIG_H */
--- fibrili.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIBRILI_H
-#define FIBRILI_H
-
-extern int     _nprocs;
-extern int     _barrier;
-extern int  *  _pids;
-
-#endif /* end of include guard: FIBRILI_H */
