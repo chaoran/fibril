7db6206fc5d2fb75a914cf14a4f530aab6c8e8d2
Chaoran Yang <chaorany@me.com>
2014-12-07 23:36:36 -0600

    Add testing framework.

---
+++ test/fib.c
@@ -0,0 +1,44 @@
+#include <fibril.h>
+
+static int fib_fast(int n)
+{
+  if (n < 2) return n;
+
+  int i = 2, x = 0, y = 0, z = 1;
+
+  do {
+    x = y;
+    y = z;
+    z = x + y;
+  } while (i++ < n);
+
+  return z;
+}
+
+__attribute__((optimize("-fno-omit-frame-pointer")))
+int fib(int n)
+{
+  if (n < 2) return n;
+
+  int x, y;
+
+  fibril_t fr;
+  fibril_init(&fr);
+
+  fibril_fork(&fr, fib, x, n - 1);
+  y = fib(n - 2);
+
+  fibril_join(&fr);
+  return x + y;
+}
+
+int main(int argc, const char *argv[])
+{
+  fibril_rt_init(FIBRIL_NPROCS_ONLN);
+  int n = 42;
+  int m = fib(n);
+
+  fibril_rt_exit();
+  return (m = fib_fast(n) ? 0 : 1);
+}
+
