30eccd2350ab66fed9a2adb19b2f9df322a5499b
Chaoran Yang <chaorany@me.com>
2015-11-07 09:55:08 -0600

    Use __ATOMIC_ACQ_REL is correct.

--- src/atomic.h
+++ src/atomic.h
@@ -2,6 +2,6 @@
 #define ATOMIC_H
 
 #define atomic_cas(ptr, cmp, val) __sync_val_compare_and_swap(ptr, cmp, val)
-#define atomic_swap(ptr, val) __atomic_exchange_n(ptr, val, __ATOMIC_RELAXED)
+#define atomic_swap(ptr, val) __atomic_exchange_n(ptr, val, __ATOMIC_ACQ_REL)
 
 #endif /* end of include guard: ATOMIC_H */
