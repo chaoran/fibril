9d5dda1fbd5636a64fe50542bee8e86197d58ea2
Chaoran Yang <chaorany@me.com>
2015-11-18 15:16:07 -0600

    Add rectmul.

--- test/rectmul.c
+++ test/rectmul.c
@@ -24,27 +24,29 @@
  *
  */
 
-#include <cilk-lib.cilkh>
 #include <stdlib.h>
 #include <stdio.h>
-#include <getoptions.h>
+#include "test.h"
 
 #define BLOCK_EDGE 16
-#define BLOCK_SIZE (BLOCK_EDGE*BLOCK_EDGE)
-typedef double block[BLOCK_SIZE];
-typedef block *pblock;
+#define BLOCK_SIZE (BLOCK_EDGE * BLOCK_EDGE)
 
-double min;
-double max;
-int count;
-double sum;
+typedef double block[BLOCK_SIZE];
+
+#ifndef BENCHMARK
+int n = 512;
+#else
+int n = 4096;
+#endif
+
+static block * A, * B, * R;
+static int x, y, z;
 
 /* compute R = R+AB, where R,A,B are BLOCK_EDGE x BLOCK_EDGE matricies
 */
-long long mult_add_block(block *A, block *B, block *R)
+static void mult_add_block(block * A, block * B, block * R)
 {
   int i, j;
-  long long flops = 0LL;
 
   for (j = 0; j < 16; j += 2) {	/* 2 columns at a time */
     double *bp = &((double *) B)[j];
@@ -125,20 +127,16 @@ long long mult_add_block(block *A, block *B, block *R)
       rp[1] = s0_1;
       rp[16] = s1_0;
       rp[17] = s1_1;
-      flops += 128;
     }
   }
-
-  return flops;
 }
 
 
 /* compute R = AB, where R,A,B are BLOCK_EDGE x BLOCK_EDGE matricies
 */
-long long multiply_block(block *A, block *B, block *R)
+static void multiply_block(block * A, block * B, block * R)
 {
   int i, j;
-  long long flops = 0LL;
 
   for (j = 0; j < 16; j += 2) {	/* 2 columns at a time */
     double *bp = &((double *) B)[j];
@@ -215,280 +213,149 @@ long long multiply_block(block *A, block *B, block *R)
       rp[1] = s0_1;
       rp[16] = s1_0;
       rp[17] = s1_1;
-      flops += 124;
     }
   }
-
-  return flops;
 }
 
 
-/* Checks if each A[i,j] of a martix A of size nb x nb blocks has value v
-*/
-
-int check_block (block *R, double v)
+int check_matrix(block * R, long x, long y, long o, double v)
 {
-  int i;
+  int a, b;
 
-  for (i = 0; i < BLOCK_SIZE; i++)
-    if (((double *) R)[i] != v)
-      return 1;
+  if (x * y == 1) {
+    /**
+     * Checks if each A[i,j] of a martix A of size nb x nb blocks has
+     * value v.
+     */
+    int i;
+    for (i = 0; i < BLOCK_SIZE; i++)
+      if (((double *) R)[i] != v)
+        return 1;
 
-  return 0;
-}
-
-cilk int check_matrix(block *R, long x, long y, long o, double v)
-{
-  int a,b;
-
-  if ((x*y) == 1)
-    return check_block(R,v);
-  else {
-    if (x>y) {
-      a = spawn check_matrix(R,x/2,y,o,v);
-      b = spawn check_matrix(R+(x/2)*o,(x+1)/2,y,o,v);
-      sync;
-    }
-    else {
-      a = spawn check_matrix(R,x,y/2,o,v);
-      b = spawn check_matrix(R+(y/2),x,(y+1)/2,o,v);
-      sync;
-    }
-  }
-  return (a+b);
-}
-
-long long add_block(block *T, block *R)
-{
-  long i;
-
-  for (i = 0; i < BLOCK_SIZE; i += 4) {
-    ((double *) R)[i] += ((double *) T)[i];
-    ((double *) R)[i + 1] += ((double *) T)[i + 1];
-    ((double *) R)[i + 2] += ((double *) T)[i + 2];
-    ((double *) R)[i + 3] += ((double *) T)[i + 3];
+    return 0;
   }
 
-  return BLOCK_SIZE;
+  if (x>y) {
+    a = check_matrix(R, x / 2, y, o, v);
+    b = check_matrix(R + (x / 2) * o,(x + 1) / 2, y, o, v);
+  } else {
+    a = check_matrix(R, x, y / 2, o, v);
+    b = check_matrix(R + (y / 2), x, (y + 1) / 2, o, v);
+  }
+
+  return a + b;
 }
 
 /* Add matrix T into matrix R, where T and R are bl blocks in size
  *
  */
-
-cilk long long add_matrix(block *T, long ot, block *R, long or, long x, long y)
+fibril void add_matrix(block * T, long ot, block * R, long or, long x, long y)
 {
-  long long flops = 0LL;
-
-  if ((x+y)==2) {
-    return add_block(T,R);
-  }
-  else {
-    if (x>y) {
-      flops += spawn add_matrix(T,ot,R,or,x/2,y);
-      flops += spawn add_matrix(T+(x/2)*ot,ot,R+(x/2)*or,or,(x+1)/2,y);
+  if (x + y == 2) {
+    long i;
+    for (i = 0; i < BLOCK_SIZE; i += 4) {
+      ((double *) R)[i + 0] += ((double *) T)[i + 0];
+      ((double *) R)[i + 1] += ((double *) T)[i + 1];
+      ((double *) R)[i + 2] += ((double *) T)[i + 2];
+      ((double *) R)[i + 3] += ((double *) T)[i + 3];
     }
-    else {
-      flops += spawn add_matrix(T,ot,R,or,x,y/2);
-      flops += spawn add_matrix(T+(y/2),ot,R+(y/2),or,x,(y+1)/2);
-    }
-
-    sync;
-    return flops;
+    return;
   }
+
+  fibril_t fr;
+  fibril_init(&fr);
+
+  if (x > y) {
+    fibril_fork(&fr, add_matrix, (T, ot, R, or, x/2, y));
+    add_matrix(T+(x/2)*ot, ot, R+(x/2)*or, or, (x+1)/2, y);
+  } else {
+    fibril_fork(&fr, add_matrix, (T, ot, R, or, x, y/2));
+    add_matrix(T+(y/2), ot, R+(y/2), or, x, (y+1)/2);
+  }
+
+  fibril_join(&fr);
 }
 
-
-void init_block(block* R, double v)
+void init_matrix(block * R, long x, long y, long o, double v)
 {
-  int i;
+  if (x + y ==2) {
+    int i;
+    for (i = 0; i < BLOCK_SIZE; i++)
+      ((double *) R)[i] = v;
+    return;
+  }
 
-  for (i = 0; i < BLOCK_SIZE; i++)
-    ((double *) R)[i] = v;
-}
-
-cilk void init_matrix(block *R, long x, long y, long o, double v)
-{
-  if ((x+y)==2)
-    init_block(R,v);
-  else  {
-    if (x>y) {
-      spawn init_matrix(R,x/2,y,o,v);
-      spawn init_matrix(R+(x/2)*o,(x+1)/2,y,o,v);
-    }
-    else {
-      spawn init_matrix(R,x,y/2,o,v);
-      spawn init_matrix(R+(y/2),x,(y+1)/2,o,v);
-    }
+  if (x > y) {
+    init_matrix(R, x/2, y, o, v);
+    init_matrix(R+(x/2) * o, (x+1)/2, y, o, v);
+  } else {
+    init_matrix(R, x, y/2, o, v);
+    init_matrix(R+(y/2), x, (y+1)/2, o, v);
   }
 }
 
-cilk long long multiply_matrix(block *A, long oa, block *B, long ob,
-    long x, long y, long z, block *R, long or, int add)
+fibril static void multiply_matrix(block * A, long oa, block * B, long ob,
+    long x, long y, long z, block * R, long or, int add)
 {
-  if ((x+y+z) == 3) {
+  if (x + y + z == 3) {
     if (add)
       return mult_add_block(A, B, R);
     else
       return multiply_block(A, B, R);
   }
-  else {
-    long long flops = 0LL;
 
-    if ((x>=y) && (x>=z)) {
-      flops += spawn multiply_matrix(A, oa, B, ob, x/2, y, z, R, or, add);
-      flops += spawn multiply_matrix(A + (x / 2) * oa, oa, B, ob,
-          (x + 1) / 2, y, z, R + (x / 2) * or, or, add);
-    }
-    else {
-      if ((y>x) && (y>z)){
-        flops += spawn multiply_matrix(A + (y / 2), oa, B + (y / 2) * ob,
-            ob, x, (y + 1) / 2, z, R, or, add);
+  fibril_t fr;
+  fibril_init(&fr);
 
-        if (SYNCHED) {
-          flops += spawn multiply_matrix(A, oa, B, ob, x, y / 2, z, R, or, 1);
-        }
-        else {
-          block *tmp = Cilk_alloca(x*z*sizeof(block));
+  if (x >= y && x >= z) {
+    fibril_fork(&fr, multiply_matrix, (A, oa, B, ob, x/2, y, z, R, or, add));
+    multiply_matrix(A+(x/2)*oa, oa, B, ob, (x+1)/2, y, z, R+(x/2)*or, or, add);
+    fibril_join(&fr);
+  } else if (y > x && y > z) {
+    fibril_fork(&fr, multiply_matrix,
+        (A+(y/2), oa, B+(y/2)*ob, ob, x, (y+1)/2, z, R, or, add));
 
-          flops += spawn multiply_matrix(A, oa, B, ob, x, y / 2, z, tmp, z, 0);
-          sync;
+    block * tmp = malloc(x * z * sizeof(block));
+    multiply_matrix(A, oa, B, ob, x, y/2, z, tmp, z, 0);
+    fibril_join(&fr);
 
-          flops += spawn add_matrix(tmp, z, R, or, x, z);
-        }
-      }
-      else {
-        flops += spawn multiply_matrix(A, oa, B, ob, x, y, z / 2, R, or, add);
-        flops += spawn multiply_matrix(A, oa, B + (z / 2), ob, x, y,
-            (z + 1) / 2, R + (z / 2), or, add);
-      }
-    }
-
-    sync;
-    return flops;
+    add_matrix(tmp, z, R, or, x, z);
+    free(tmp);
+  } else {
+    fibril_fork(&fr, multiply_matrix, (A, oa, B, ob, x, y, z/2, R, or, add));
+    multiply_matrix(A, oa, B+(z/2), ob, x, y, (z+1)/2, R+(z/2), or, add);
+    fibril_join(&fr);
   }
 }
 
-cilk int run(long x, long y, long z, int check)
-{
-  block *A, *B, *R;
-  long long flops;
-  double f;
-  Cilk_time tm_begin, tm_elapsed;
-  Cilk_time wk_begin, wk_elapsed;
-  Cilk_time cp_begin, cp_elapsed;
+void init() {
+  x = n / BLOCK_EDGE;
+  y = n / BLOCK_EDGE;
+  z = n / BLOCK_EDGE;
 
-  A = malloc(x*y * sizeof(block));
-  B = malloc(y*z * sizeof(block));
-  R = malloc(x*z * sizeof(block));
+  A = malloc(x * y * sizeof(block));
+  B = malloc(y * z * sizeof(block));
+  R = malloc(x * z * sizeof(block));
 
-  spawn init_matrix(A, x, y, y, 1.0);
-  spawn init_matrix(B, y, z, z, 1.0);
-  spawn init_matrix(R, x, z, z, 0.0);
-  sync;
+  init_matrix(A, x, y, y, 1.0);
+  init_matrix(B, y, z, z, 1.0);
+}
 
-  /* Timing. "Start" timers */
-  sync;
-  cp_begin = Cilk_user_critical_path;
-  wk_begin = Cilk_user_work;
-  tm_begin = Cilk_get_wall_time();
+void prep() {
+  init_matrix(R, x, z, z, 0.0);
+}
 
-  flops = spawn multiply_matrix(A,y,B,z,x,y,z,R,z,0);
-  sync;
+void test() {
+  multiply_matrix(A, y, B, z, x, y, z, R, z, 0);
+}
 
-  /* Timing. "Stop" timers */
-  tm_elapsed = Cilk_get_wall_time() - tm_begin;
-  wk_elapsed = Cilk_user_work - wk_begin;
-  cp_elapsed = Cilk_user_critical_path - cp_begin;
-
-  f = (double) flops / 1.0E6 / Cilk_time_to_sec(tm_elapsed);
-
-  if (check) {
-    check = spawn check_matrix(R, x, z, z, y*16);
-    sync;
-  }
-
-  if (min>f) min=f;
-  if (max<f) max=f;
-  sum+=f;
-  count++;
-
-  if (check)
+int verify() {
+#ifndef BENCHMARK
+  if (check_matrix(R, x, z, z, y * 16)) {
     printf("WRONG RESULT!\n");
-  else {
-    printf("\nCilk Example: rectmul\n");
-    printf("	      running on %d processor%s\n\n", Cilk_active_size,
-        Cilk_active_size > 1 ? "s" : "");
-    printf("Options: x = %ld\n", BLOCK_EDGE*x);
-    printf("         y = %ld\n", BLOCK_EDGE*y);
-    printf("         z = %ld\n\n", BLOCK_EDGE*z);
-    printf("Mflops        = %4f \n", (double) flops / 1.0E6 /
-        Cilk_time_to_sec(tm_elapsed));
-    printf("Running time  = %4f s\n", Cilk_wall_time_to_sec(tm_elapsed));
-    printf("Work          = %4f s\n", Cilk_time_to_sec(wk_elapsed));
-    printf("Critical path = %4f s\n\n", Cilk_time_to_sec(cp_elapsed));
-  }
-
-  free(A);
-  free(B);
-  free(R);
+    return 1;
+  };
+#endif
 
   return 0;
-}
-
-
-int usage(void) {
-  fprintf(stderr, "\nUsage: rectmul [<cilk-options>] [<options>]\n\n");
-  return 1;
-}
-
-char *specifiers[] = { "-x", "-y", "-z", "-c", "-benchmark", "-h", 0};
-int opt_types[] = {INTARG, INTARG, INTARG, BOOLARG, BENCHMARK, BOOLARG, 0 };
-
-cilk int cilk_main(int argc, char *argv[])
-{
-  int x, y, z, benchmark, help, t, check;
-
-  /* standard benchmark options */
-  x = 512;
-  y = 512;
-  z = 512;
-  check = 0;
-
-  get_options(argc, argv, specifiers, opt_types, &x, &y, &z, &check,
-      &benchmark, &help);
-
-  if (help) return usage();
-
-  if (benchmark) {
-    switch (benchmark) {
-      case 1:      /* short benchmark options -- a little work*/
-        x = 256;
-        y = 256;
-        z = 256;
-        break;
-      case 2:      /* standard benchmark options*/
-        x = 512;
-        y = 512;
-        z = 512;
-        break;
-      case 3:      /* long benchmark options -- a lot of work*/
-        x = 2048;
-        y = 2048;
-        z = 2048;
-        break;
-    }
-  }
-
-  x = x/BLOCK_EDGE;
-  y = y/BLOCK_EDGE;
-  z = z/BLOCK_EDGE;
-
-  if (x<1) x=1;
-  if (y<1) y=1;
-  if (z<1) z=1;
-
-  t = spawn run(x,y,z,check);
-
-  return t;
 }
