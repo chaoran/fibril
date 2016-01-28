d2171432dd748a7e2a89662c8d2da807dc4edbc6
Chaoran Yang <chaorany@me.com>
2016-01-28 16:30:51 -0600

    [BF] Use "volatile" in mutex impl.

--- src/mutex.c
+++ src/mutex.c
@@ -4,7 +4,7 @@
 #define NULL ((void *) 0)
 #define spin_wait(x) while (!(x)) __asm__ ( "pause" ::: "memory" )
 
-void mutex_lock(mutex_t ** mutex, mutex_t * node)
+void mutex_lock(mutex_t * volatile * mutex, mutex_t * node)
 {
   node->next = NULL;
   mutex_t * prev = sync_swap(mutex, node);
@@ -16,14 +16,14 @@ void mutex_lock(mutex_t ** mutex, mutex_t * node)
   }
 }
 
-int mutex_trylock(mutex_t ** mutex, mutex_t * node)
+int mutex_trylock(mutex_t * volatile * mutex, mutex_t * node)
 {
   node->next = NULL;
   mutex_t * prev = sync_cas(mutex, NULL, node);
   return (prev == NULL);
 }
 
-void mutex_unlock(mutex_t ** mutex, mutex_t * node)
+void mutex_unlock(mutex_t * volatile * mutex, mutex_t * node)
 {
   if (node->next == NULL) {
     if (node == sync_cas(mutex, node, NULL)) {
--- src/mutex.h
+++ src/mutex.h
@@ -8,8 +8,8 @@ typedef struct _mutex_t {
   volatile char flag;
 } mutex_t __attribute__((aligned(128)));
 
-void mutex_lock   (mutex_t ** mutex, mutex_t * node);
-int  mutex_trylock(mutex_t ** mutex, mutex_t * node);
-void mutex_unlock (mutex_t ** mutex, mutex_t * node);
+void mutex_lock   (mutex_t * volatile * mutex, mutex_t * node);
+int  mutex_trylock(mutex_t * volatile * mutex, mutex_t * node);
+void mutex_unlock (mutex_t * volatile * mutex, mutex_t * node);
 
 #endif /* end of include guard: MUTEX_H */
